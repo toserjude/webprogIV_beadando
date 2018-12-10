de606a84dcb6dbee308a37fa9e9b0e0634110c82
Tősér Judit <toser.jude@gmail.com>
2018-12-10 16:22:04 +0100

    bedding to posting

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is a laravel application from the "laravel from scratch" youtube series.</p>   
+<div class="jumbotron text-center">
+    <h1 class="display-4">{{$title}}</h1>
+    <p class="lead">This is a laravel application from the "laravel from scratch" youtube series.</p>
+    <hr class="my-4">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Log In</a>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Register</a>
+</div>
+  
 @endsection
