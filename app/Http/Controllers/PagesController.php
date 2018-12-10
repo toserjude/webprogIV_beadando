fd64b87ad9410b6426a4cd70006ba9f19abcb1e9
Tősér Judit <toser.jude@gmail.com>
2018-12-10 10:59:04 +0100

    practice basics

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'Welcome to Laravel';
+        // return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about(){
+        $title = 'About Us';
+        return view('pages.about')->with('title', $title);
+        // return view('pages.about');
+    }
+
+    public function services(){
+        $data = array(
+            'title' => 'Services',
+            'services' => [
+                'WebDesign',
+                'Programming',
+                'SEO'
+            ]
+        );
+        return view('pages.services')->with($data);
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>This is a laravel application from the "laravel from scratch" youtube series.</p>   
+@endsection
